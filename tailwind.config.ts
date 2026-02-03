de1b9d32478d7968bcc7caa88c8436065c1f42cd
yanntavares <yanntavares123@gmail.com>
2026-02-03 17:07:23 -0300

    feat: add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,11 @@ export default {
       backgroundImage: {
         "stripped-pattern" : "repeating-linear-gradient(45deg, #e5e7eb, #e5e7eb 10px, #f3f4f6 10px, #f3f4f6 20px)",
       },
+      fontFamily: {
+        'pokemon': ['pokemon-solid', 'sans-serif'],
+        'hollow': ['pokemon-hollow', 'sans-serif'],
+        sans: ['var(--font-merriweather)', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
